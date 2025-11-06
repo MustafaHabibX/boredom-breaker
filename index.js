966729c222a3e59c9c5820dac092c86df1e3ec43
MustafaHabibX <136514653+MustafaHabibX@users.noreply.github.com>
2025-11-06 10:38:04 +0430

    feat: add activity filtering by type and participants with POST request

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
   try {
@@ -16,6 +18,32 @@ app.get("/", async (req, res) => {
     console.error("Failed to make request:", error.message);
     res.render("index.ejs", {
       error: error.message,
+    });
+  }
+});
+
+app.post("/", async (req, res) => {
+  try {
+    // Using the public API via Axios with specific parameters specified with queries
+    const selectedType = req.body.type;
+    const selectedParticipants = parseInt(req.body.participants);
+
+    const specificResponse = await axios.get(
+      `https://bored-api.appbrewery.com/filter/?type=${selectedType}&participants=${selectedParticipants}`
+    );
+
+    // Storing the data that came from the API
+    const result = specificResponse.data;
+
+    // Calculation for randomly selecting one of the returned answers from the array of result that we had
+    let randomNumber = Math.floor(Math.random() * result.length);
+
+    // Rnadomly sending of of the results
+    res.render("index.ejs", { data: result[randomNumber] });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: "No activities that match your criteria.",
     });
   }
 });
