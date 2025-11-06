f9c3313ad6fe91cc20a1f3f4bfa6ed89bf4e7440
MustafaHabibX <136514653+MustafaHabibX@users.noreply.github.com>
2025-11-06 10:33:03 +0430

    feat: fetch and display random activity on page load using Bored API

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import express from "express";
 
 const app = express();
@@ -5,8 +6,18 @@ const port = 3000;
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+app.get("/", async (req, res) => {
+  try {
+    // Using axios.get to fetch a random activity from the bored public API
+    const response = await axios.get("https://bored-api.appbrewery.com/random");
+    const result = response.data;
+    res.render("index.ejs", { data: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
 });
 
 // Server is listening on the specified port
